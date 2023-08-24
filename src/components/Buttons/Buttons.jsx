e872ecb1b093d9079218a0858a70f83f3b2b268c
Sabrine Dhaliwal <sabrine.dhaliwal@gmail.com>
2023-08-24 10:05:46 -0700

    initial commit

---
+++ src/components/Buttons/Buttons.jsx
@@ -0,0 +1,7 @@
+import react from "react";
+
+function Button({ text, modifer }) {
+  return <button className={`btn btn${modifer}`}>{text}</button>;
+}
+
+export default Button;
---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";  
+
+function HomePage() {
+
+    
+    return (
+<div className="body">
+
+<h1>Track Your Journey in Brazilian Jiu-Jitsu</h1>
+</div>
+    );
+}
+
+export default HomePage; 
