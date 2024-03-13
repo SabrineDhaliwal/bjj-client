5d388f69d5e058a2deb2d53f64a1ecaacbbea1c2
Sabrine Dhaliwal <sabrine.dhaliwal@gmail.com>
2024-03-13 11:25:46 -0700

    update config file

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  assetsInclude: ['**/*.jpg'],
-  define: {
-    'import.meta.env.': {},
-  },
-})
+// export default defineConfig({
+//   plugins: [react()],
+//   assetsInclude: ['**/*.jpg'],
+//   define: {
+//     'import.meta.env.': {},
+//   },
+// })
+
+import { defineConfig, loadEnv } from 'vite'
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    define: {
+      'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
+    }
+  })
+}
