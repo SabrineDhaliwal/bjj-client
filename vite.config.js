e7f3341a503de026bee4455116b7d9fd5a94c8ff
Sabrine Dhaliwal <sabrine.dhaliwal@gmail.com>
2024-03-13 13:17:07 -0700

    test build

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,52 @@
 // import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-//   assetsInclude: ['**/*.jpg'],
-//   define: {
-//     'import.meta.env.': {},
-//   },
+export default defineConfig({
+  plugins: [react()],
+  // assetsInclude: ['**/*.jpg'],
+  // define: {
+  //   'import.meta.env.': {},
+  // },
+})
+
+
+
+
+// this didn't work
+
+// export default ({ mode }) => {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+//   return defineConfig({
+//     plugins: [react()],
+//     assetsInclude: ["**/*.jpg"],
+//     define: {
+//       "process.env.BASE_URL": JSON.stringify(process.env.VITE_BASE_URL),
+//     },
+//   });
+// };
+
+// this didn't work 
+
+// export default defineConfig(({mode})=> {
+//   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+//     return defineConfig({
+//       plugins: [react()],
+//       assetsInclude: ["**/*.jpg"],
+//       define: {
+//         "process.env.BASE_URL": JSON.stringify(process.env.VITE_BASE_URL),
+//       },
+//     });
 // })
 
-import { defineConfig, loadEnv } from 'vite'
-export default ({ mode }) => {
-  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  return defineConfig({
-    define: {
-      'process.env.VITE_BASE_URL': JSON.stringify(process.env.VITE_BASE_URL)
-    }
-  })
-}
+// export default ({ mode })=> {
+//   const env = loadEnv(mode, process.cwd(), '');
+
+//   return defineConfig({
+//     base: env.VITE_BASE_URL || '/',
+//     define: {
+//       'process.env.VITE_BASE_URL': JSON.stringify(env.VITE_BASE_URL),
+//     },
+//   })
+// }
